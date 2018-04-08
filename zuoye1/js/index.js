a97c347de98bf0aad174d7e1caa4d63b5a86f911
Xue Zhou <xuezhouapplication@gmail.com>
2018-04-08 17:05:51 +0800

    fix the uptop

--- zuoye1/js/index.js
+++ zuoye1/js/index.js
@@ -19,7 +19,7 @@ function checkAll() {
                   var all=document.getElementById('all');  
                   var one=document.getElementsByName('checkname[]');
                   for(var i=0;i<one.length;i++){  
-                  one[i].checked=all.checked; 
+                     one[i].checked=all.checked; 
                   }  
 } 
 
@@ -42,45 +42,34 @@ function formatDate(date){
         return y+"-"+m+"-"+d+" "+h+":"+m1+":"+s;
 }
 
-//The JS used to deal with ajax
-function ajaxRequest() {
-     var blogArray = [];
-    //ajax to retrieve all the blogs 
-     var xhttp = new XMLHttpRequest();
-     xhttp.onreadystatechange = function() {
-     if (this.readyState == 4 && this.status == 200) {
-      var obj = JSON.parse(this.responseText);
-      
+///////*********************************************************************************************************
 
-      //obj['result'] is the returned array which store the blog details (original array)
-      var returnedArray = obj['result'];
-
-      //The JS to move the blog to the first 
-      function upfirst(clickedIndex,arr){
-        //unset all the rank as '0'
-        for (var j = 0; j < arr.length; j++) {
-            arr[j]['rank']= 0;
-        }
-        arr[clickedIndex]['rank'] = 5;
-        
-        //reorde the array
-        arrayOrganize(arr);
-      }
+//The JS used to deal with ajax and get all the data;
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+      if(this.readyState == 4 && this.status == 200) {
+         var obj = JSON.parse(this.responseText);
+        //obj['result'] is the returned array which store the blog details (original array)
+         returnedArray = obj['result'];
+      }//ajax request ends 
+  };
+  xhttp.open("GET", "http://localhost:8002/api/getBlogs", true);
+  xhttp.send();
 
 
-      //The JS used to sort the arrays based on a property
-      function objectArraySort(keyName) {
-       return function (objectN, objectM) {
-        var valueN = objectN[keyName]
-        var valueM = objectM[keyName]
-        if (valueN < valueM) return 1
-        else if (valueN > valueM) return -1
-        else return 0
-       }
-      }
 
-      //use this funciton to finailize the array order.
-      function arrayOrganize(arr){
+//The JS used to sort the arrays based on a property
+function objectArraySort(keyName) {
+         return function (objectN, objectM) {
+          var valueN = objectN[keyName]
+          var valueM = objectM[keyName]
+          if (valueN < valueM) return 1
+          else if (valueN > valueM) return -1
+          else return 0 }
+}
+
+//use this funciton to finailize the array order.
+function arrayOrganize(arr){
         //1. first step, sort the array by the modifyTime.
         arr = arr.sort(objectArraySort('modifyTime'));
         //2. identify the top first and move it to the first.
@@ -91,10 +80,11 @@ function ajaxRequest() {
             break;
           }
         }
-        arr.unshift(removed);
+        arr.unshift(removed); //new array which is ready for render. 
 
         var liStr = '';
         for(var i = 0; i<arr.length; i++) {
+              var index = i;
               liStr +='<li class="bloglist-item" style="display: block;">';
               liStr +='<input type="checkbox" name="checkname[]" value="'+i+'"><span>'+arr[i]['title']+'</span>';
               liStr +='<div class="blog-btn-wrapper">';
@@ -103,72 +93,82 @@ function ajaxRequest() {
               liStr +='<button class="dropbtn">更多&nbsp;<i class="fas fa-caret-down"></i></button>';
               liStr +='<div class="dropdown-content" >';
               liStr +='<a onclick={deletepost();}>删除</a>';
-              liStr +='<a class="up-btn">置顶</a>'; //////////////!!!!!!!!!!!!!!!
+              liStr +='<a class="up-btn" id="'+ index +'" onclick="uppost('+ index +')">置顶</a>'; //////////////!!!!!!!!!!!!!!!
               liStr +='</div></div></div>';
               liStr +='<div class="blog-meta-wrapper">';
               liStr +='<p>'+formatDate(arr[i]['modifyTime'])+'&nbsp;&nbsp;&nbsp;阅读'+arr[i]['accessCount']+'&nbsp;&nbsp;&nbsp;评论'+arr[i]['commentCount']+'</p>';
               liStr +='</div>';
               liStr +='</li>';
             } //for loop ends here
-
         document.getElementById('bloglist').innerHTML = liStr;
+        //console.log(arr+'from arrayOrganize FUNCTION');
+        return arr;
+}//arrayOrganize FUNCTION ends here
 
-      }//arrayOrganize function ends here
-      
-      arrayOrganize(returnedArray);
-      //1. first step, sort the array by the modifyTime.
-      // returnedArray =returnedArray.sort(objectArraySort('modifyTime'));
-      //2. identify the top first and move it to the first.
-      // for (var i = 0; i < returnedArray.length; i++) {
-      //     if (returnedArray[i]['rank'] === 5) {
-      //       var removed = returnedArray[i];
-      //       returnedArray.splice(i, 1);
-      //       break;
-      //     }
-      //   }
-      // returnedArray.unshift(removed);
 
-      //initialize the string to RENDER the list; before the rendering, the array should be determined.
-      // var liStr = '';
+function arrayOrganize2(arr){
+        //1. first step, sort the array by the modifyTime.
+        arr = arr.sort(objectArraySort('modifyTime'));
+        // //2. identify the top first and move it to the first.
+        // for (var i = 0; i < arr.length; i++) {
+        //   if (arr[i]['rank'] === 5) {
+        //     var removed = arr[i];
+        //     arr.splice(i, 1);
+        //     break;
+        //   }
+        // }
+        // arr.unshift(removed); //new array which is ready for render. 
 
-            // for(var i = 0; i<obj['result'].length; i++) {
-            //    blogArray.push(obj['result'][i]);
-            // }
-            // alert(blogArray.length);
+        var liStr = '';
+        for(var i = 0; i<arr.length; i++) {
+              var index = i;
+              liStr +='<li class="bloglist-item" style="display: block;">';
+              liStr +='<input type="checkbox" name="checkname[]" value="'+i+'"><span>'+arr[i]['title']+'</span>';
+              liStr +='<div class="blog-btn-wrapper">';
+              liStr +='<button>编辑</button>';
+              liStr +='<div class="dropdown">';
+              liStr +='<button class="dropbtn">更多&nbsp;<i class="fas fa-caret-down"></i></button>';
+              liStr +='<div class="dropdown-content" >';
+              liStr +='<a onclick={deletepost();}>删除</a>';
+              liStr +='<a class="up-btn" id="'+ index +'" onclick="uppost('+ index +')">置顶</a>'; //////////////!!!!!!!!!!!!!!!
+              liStr +='</div></div></div>';
+              liStr +='<div class="blog-meta-wrapper">';
+              liStr +='<p>'+formatDate(arr[i]['modifyTime'])+'&nbsp;&nbsp;&nbsp;阅读'+arr[i]['accessCount']+'&nbsp;&nbsp;&nbsp;评论'+arr[i]['commentCount']+'</p>';
+              liStr +='</div>';
+              liStr +='</li>';
+            } //for loop ends here
+        document.getElementById('bloglist').innerHTML = liStr;
+        //console.log(arr+'from arrayOrganize FUNCTION');
+        return arr;
+}//arrayOrganize FUNCTION ends here
 
-            // for(var i = 0; i<returnedArray.length; i++) {
-            //   liStr +='<li class="bloglist-item" style="display: block;">';
-            //   liStr +='<input type="checkbox" name="checkname[]" value="1"><span>'+returnedArray[i]['title']+'</span>';
-            //   liStr +='<div class="blog-btn-wrapper">';
-            //   liStr +='<button>编辑</button>';
-            //   liStr +='<div class="dropdown">';
-            //   liStr +='<button class="dropbtn">更多&nbsp;<i class="fas fa-caret-down"></i></button>';
-            //   liStr +='<div class="dropdown-content" >';
-            //   liStr +='<a onclick={deletepost();}>删除</a>';
-            //   liStr +='<a id="up-btn">置顶</a>';
-            //   liStr +='</div></div></div>';
-            //   liStr +='<div class="blog-meta-wrapper">';
-            //   liStr +='<p>'+formatDate(returnedArray[i]['modifyTime'])+'&nbsp;&nbsp;&nbsp;阅读'+returnedArray[i]['accessCount']+'&nbsp;&nbsp;&nbsp;评论'+returnedArray[i]['commentCount']+'</p>';
-            //   liStr +='</div>';
-            //   liStr +='</li>';
-            // } //for loop ends here
-           // document.getElementById('bloglist').innerHTML = liStr;
-    }//ajax request ends 
-  };
-  xhttp.open("GET", "http://localhost:8002/api/getBlogs", true);
-  xhttp.send();
-}
+
+
 
 window.onload=function(){
-     ajaxRequest();
+     console.log(returnedArray,'original');
+     arrayOrganize(returnedArray);
+     console.log(returnedArray);
 }
 
-var bloglist = document.getElementsByClassName('bloglist-item');
-console.log(bloglist.length);
 
+//The script to push the post to the upfirst
+var uppost = function(index){
+  // alert(index);
+  if(returnedArray[index]['rank'] === 5){ //first one already
+    returnedArray[index]['rank'] = 1;
+    console.log(returnedArray,'new array if untop');
+    arrayOrganize2(returnedArray);
+  }
+  else{ //not first one
+    for (var j = 0; j < returnedArray.length; j++) {
+      returnedArray[j]['rank']= 0;
+    } 
+      returnedArray[index]['rank'] = 5;
+      arrayOrganize(returnedArray);
+      console.log(returnedArray,'new array');
+  }
+  
+}
 
-//The JS to delete the post
-var deletepost = function(){
-  alert('test2');
-} 
 
